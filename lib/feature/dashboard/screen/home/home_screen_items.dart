24e19e027d40c038c162e236c0c9ff8c4214c335
rojanthapa <rojaneverest@gmail.com>
2023-11-07 09:21:37 +0545

    test commit

---
+++ lib/feature/dashboard/screen/home/home_screen_items.dart
@@ -0,0 +1,14 @@
+import 'package:flamingo/feature/dashboard/screen/home/Brand_screen.dart';
+import 'package:flamingo/feature/dashboard/screen/home/Searchscreen.dart';
+import 'package:flamingo/feature/dashboard/screen/home/main_home_screen.dart';
+import 'package:flamingo/feature/dashboard/screen/home/me_screen.dart';
+import 'package:flamingo/feature/dashboard/screen/home/wishlist_screen.dart';
+import 'package:flutter/material.dart';
+
+List<Widget> homescreenitems = [
+  const MainHomeScreen(),
+  const SearchScreen(),
+  const BrandScreen(),
+  const WishlistScreen(),
+  const MeScreen(),
+];

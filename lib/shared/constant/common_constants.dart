4224c8651888e06a3a3eeacc524606c151e554f6
pragyann <pragyan@noveltytechnology.com>
2024-01-07 11:35:35 +0545

    feat: set up khalti integration

--- lib/shared/constant/common_constants.dart
+++ lib/shared/constant/common_constants.dart
@@ -1,3 +1,6 @@
 class CommonConstants {
   static const int otpLength = 4;
+  static const khaltiPaymentCode = "KHALTI";
+  static const khaltiPublicKey =
+      "test_public_key_7e4095f17b5d47f5ae8dd889dc1bfff0";
 }

35d8d55178fdb0ee3bb329bb4e2a0fe350a48047
pragyann1 <pragyan@noveltytechnology.com>
2024-05-05 21:29:42 +0545

    fix: firebase cloud messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flamingo/app.dart';
 import 'package:flamingo/feature/theme/theme_service.dart';
 import 'package:flamingo/navigation/navigation_service.dart';
 import 'di/service_locator.dart' as di;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  log("FCMToken $fcmToken");
+
   await di.setUpServiceLocator();
   await initApp();
 
